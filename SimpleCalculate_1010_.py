297764511e4a32bbd3cd70d5dbd09a73ded1530e
Gabriel <gabrielgiliotti@hotmail.com>
2019-04-10 17:29:55 -0300

    Mais 3 em ordem

---
+++ SimpleCalculate_1010_.py
@@ -0,0 +1,8 @@
+cod1, unit1, price1 = input().split(" ")
+cod2, unit2, price2 = input().split(" ")
+unit1 = int(unit1)
+unit2 = int(unit2)
+price1 = float(price1)
+price2 = float(price2)
+total = (unit1*price1) + (unit2*price2)
+print("VALOR A PAGAR: R$ %.2f" % total, end="\n")
---
+++ Sphere_1011_.py
@@ -0,0 +1,3 @@
+R = int(input())
+Vol = (4.0/3)*3.14159*R*R*R
+print("VOLUME = %.3f" % Vol, end="\n")
